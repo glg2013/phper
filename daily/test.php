a881585a940c67b3bfddb8496b5d15195484f283
fairy_shrimp <fengniancong@163.com>
2024-12-07 19:57:43 +0800

    测试

--- daily/test.php
+++ daily/test.php
@@ -1,7 +1,47 @@
 <?php
 
-$a = 111;
-$test = <<<EOC
-    $a bc
-EOC;
-echo $test;
+//$a = 111;
+//$test = <<<EOC
+//    $a bc
+//EOC;
+//echo $test;
+
+
+//$list = [];
+//for ($i = 100; $i <= 1000; $i++) {
+//    if ($i % 10 == 9 && $i % 9 == 8 && $i % 8 == 7) {
+//        array_push($list, $i);
+//    }
+//}
+//
+//print_r($list);
+
+//$a = 3;
+//$b = $a << 3;
+//echo $b;
+
+//$a = 2560;
+//$b = 0.1;
+//
+//echo gettype($a);
+//echo PHP_EOL;
+//echo gettype($b);
+//echo PHP_EOL;
+//
+//echo $a * $b;
+
+
+//$a = 2;
+//$b = $a++;
+//echo $a;
+
+//$a = array_sum(explode(' ', microtime()));
+//echo $a;
+
+//$str = 'woxihuanPHP盛世中华';
+//$list = mb_str_split(strtolower($str));
+//print_r($list);
+
+$list = [1, 2, 3, 4, 'hello', 'world'];
+$a = serialize($list);
+var_dump($a);
